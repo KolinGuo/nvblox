023d1c31b7279f5db7610f69565e37bc59817432
Kolin Guo <guokolin@gmail.com>
2026-01-17 11:56:26 -0800

    [build-fix] Update in-place build

--- nvblox_torch/nvblox_torch/lib/utils.py
+++ nvblox_torch/nvblox_torch/lib/utils.py
@@ -10,6 +10,7 @@
 #
 from typing import Any
 import os
+import glob
 
 import torch
 
@@ -22,9 +23,59 @@ def get_module_path() -> str:
 
 
 def get_nvblox_py_library_path() -> str:
-    """Get the path to the nvblox_torch .so library."""
-    path = os.path.join(get_module_path(), 'nvblox_torch/cpp/libpy_nvblox.so')
-    return path
+    """Get the path to the nvblox_torch .so library.
+
+    This function searches for the library in multiple locations to support:
+    1. Installed packages (library in lib/nvblox_torch/cpp/)
+    2. In-place/editable builds (library in lib/nvblox_torch/cpp/)
+    3. Build directory layouts
+
+    Returns:
+        Path to libpy_nvblox.so
+
+    Raises:
+        FileNotFoundError: If the library cannot be found in any location
+    """
+    module_path = get_module_path()
+
+    # List of possible library locations (in order of preference)
+    possible_paths = [
+        # Standard installed/in-place location
+        os.path.join(module_path, 'nvblox_torch/cpp/libpy_nvblox.so'),
+        # Alternative paths for different build configurations
+        os.path.join(module_path, 'nvblox_torch', 'cpp', 'libpy_nvblox.so'),
+        # Direct path (for cases where lib/ is the library directory)
+        os.path.join(module_path, 'libpy_nvblox.so'),
+    ]
+
+    # Also check for libraries in build directories relative to the source
+    source_root = os.path.dirname(os.path.dirname(module_path))
+    build_patterns = [
+        os.path.join(source_root, 'build', '**', 'libpy_nvblox.so'),
+        os.path.join(source_root, 'build', '**', 'cpp', 'libpy_nvblox.so'),
+    ]
+
+    # First, check explicit paths
+    for path in possible_paths:
+        if os.path.exists(path):
+            return path
+
+    # Then, search build directories using glob
+    for pattern in build_patterns:
+        matches = glob.glob(pattern, recursive=True)
+        if matches:
+            # Return the first match
+            return matches[0]
+
+    # If we get here, we couldn't find the library
+    search_locations = '\n  - '.join(possible_paths + build_patterns)
+    raise FileNotFoundError(
+        f"Could not find libpy_nvblox.so. Searched in:\n  - {search_locations}\n"
+        "Please ensure the package is built. For in-place builds, run:\n"
+        "  pip install -e .\n"
+        "Or build with CMake:\n"
+        "  mkdir build && cd build && cmake .. && make"
+    )
 
 
 def get_nvblox_torch_class(class_name: str) -> Any:
